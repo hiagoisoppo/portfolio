3f985d9920e14f86ff711e05f148f05553c9f4bf
Hiago Isoppo <hiagoisoppotrajano@gmail.com>
2024-05-17 19:33:54 -0300

    test1

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,10 +7,7 @@ import type { Viewport } from 'next'
 export const viewport: Viewport = {
   width: 'device-width',
   initialScale: 1,
-  maximumScale: 1,
   userScalable: false,
-  // Also supported by less commonly used
-  // interactiveWidget: 'resizes-visual',
 };
 
 export const metadata: Metadata = {
--- src/data/projects.ts
+++ src/data/projects.ts
@@ -1,50 +1,62 @@
 export const projects = [
   {
-    title: 'Timer and Stopwatch',
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Lorem ipsum dolor sit amet, consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",    
-    deploy: 'https://sandrords.github.io/rds-games/',
-    repository: 'TESTANDO',
-    preview: 'https://raw.githubusercontent.com/hiagoisoppo/timer-and-stopwatch/main/public/Preview.png',
-    previewHover: '',
+    title: "Timer and Stopwatch",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    deploy: "https://sandrords.github.io/rds-games/",
+    repository: "TESTANDO",
+    preview:
+      "https://raw.githubusercontent.com/hiagoisoppo/timer-and-stopwatch/main/public/Preview.png",
+    previewHover: "",
   },
   {
-    title: 'Gourmet Guidance',
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Lorem ipsum dolor sit amet, consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",    
-    deploy: '',
-    repository: 'TESTANDO',
-    preview: 'https://raw.githubusercontent.com/hiagoisoppo/gourmet-guidance/main-group-5/public/Preview.png',
-    previewHover: '',
+    title: "Gourmet Guidance",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    deploy: "",
+    repository: "TESTANDO",
+    preview:
+      "https://raw.githubusercontent.com/hiagoisoppo/gourmet-guidance/main-group-5/public/Preview.png",
+    previewHover: "",
   },
   {
-    title: 'Hit News',
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Lorem ipsum dolor sit amet, consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",    
-    deploy: 'TESTANDO',
-    repository: 'TESTANDO',
-    preview: 'https://raw.githubusercontent.com/hiagoisoppo/hit-news/main/public/Preview.png',
-    previewHover: '',
+    title: "Hit News",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    deploy: "TESTANDO",
+    repository: "TESTANDO",
+    preview:
+      "https://raw.githubusercontent.com/hiagoisoppo/hit-news/main/public/Preview.png",
+    previewHover: "",
   },
   {
-    title: 'Exchange House',
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Lorem ipsum dolor sit amet, consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",    
-    deploy: 'TESTANDO',
-    repository: 'TESTANDO',
-    preview: 'https://raw.githubusercontent.com/hiagoisoppo/exchange-house/master/public/Preview.png',
-    previewHover: '',
+    title: "Exchange House",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    deploy: "TESTANDO",
+    repository: "TESTANDO",
+    preview:
+      "https://raw.githubusercontent.com/hiagoisoppo/exchange-house/master/public/Preview.png",
+    previewHover: "",
   },
   {
-    title: 'Blog Api',
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Lorem ipsum dolor sit amet, consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",    
-    deploy: '',
-    repository: 'TESTANDO',
-    preview: 'https://raw.githubusercontent.com/hiagoisoppo/api-blog/hiago-isoppo-blogs-api/public/Preview.png',
-    previewHover: '',
+    title: "Blog Api",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    deploy: "",
+    repository: "TESTANDO",
+    preview:
+      "https://raw.githubusercontent.com/hiagoisoppo/api-blog/hiago-isoppo-blogs-api/public/Preview.png",
+    previewHover: "",
   },
   {
-    title: 'Trybe Futebol Clube',
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Lorem ipsum dolor sit amet, consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",    
-    deploy: '',
-    repository: 'TESTANDO',
-    preview: 'https://raw.githubusercontent.com/hiagoisoppo/trybe-futebol-club/hiago-isoppo-sd-032-project-trybe-futebol-clube/public/Preview.png',
-    previewHover: '',
+    title: "Trybe Futebol Clube",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    deploy: "",
+    repository: "TESTANDO",
+    preview:
+      "https://raw.githubusercontent.com/hiagoisoppo/trybe-futebol-club/hiago-isoppo-sd-032-project-trybe-futebol-clube/public/Preview.png",
+    previewHover: "",
   },
 ];
